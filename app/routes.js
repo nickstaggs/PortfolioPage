cf7e8e1d06a3fe4e546093dfbd14c8f26372e15e
Nick Staggs <nick.c.staggs@gmail.com>
2017-02-17 07:37:41 -0500

    Actually fixed routing, resolved dirname

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,6 @@
 module.exports = function (app) {
   app.get('*', function (req, res) {
-        res.sendFile('public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    path.resolve(__dirname, '.../public')
+    res.sendFile(__dirname + '/index.html'); // load the single view file (angular will handle the page changes on the front-end)
   });
 };
