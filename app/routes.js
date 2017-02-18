877cb4b6149a22f6316aac38625f9e32e6c630d8
Nick Staggs <nick.c.staggs@gmail.com>
2017-02-18 14:22:29 -0500

    changed directory path in routes.js

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,9 @@
 module.exports = function (app) {
   var path = require('path');
   app.get('*', function (req, res) {
-    path.resolve(__dirname, '.../public')
-    res.sendFile(__dirname + '/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    //path.resolve(__dirname, '.../public')
+    res.sendFile('..\\public\\index.html'); 
+
+    console.log(__dirname + " " + process.cwd());// load the single view file (angular will handle the page changes on the front-end)
   });
 };
