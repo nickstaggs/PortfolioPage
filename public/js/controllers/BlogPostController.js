8fe67f38eafc832fdb704847f4a6cc93e6530ce9
Nick Staggs <nick.c.staggs@gmail.com>
2017-07-11 14:49:06 -0400

    changed time format

--- public/js/controllers/BlogPostController.js
+++ public/js/controllers/BlogPostController.js
@@ -1,9 +1,12 @@
+var moment = require('moment');
+
 angular.module('BlogPostCtrl',[]).controller('BlogPostController',
   ['$scope','$http', '$routeParams', function($scope, $http, $routeParams) {
     var initPost = function() {
       console.log('/blogposts/' + $routeParams.blogId);
       $http.get('/blogposts/' + $routeParams.blogId).then(function successCallBack(response) {
         console.log('inside');
+        response.data.datePosted = moment(response.data.datePosted).format('MMMM Do YYYY, h:mm:ss a');
         $scope.post = response.data;
         console.log(response);
       },
