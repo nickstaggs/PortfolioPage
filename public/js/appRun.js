d316da5ae5258c92a7a8d0bcaa238432bbdf3ad7
Nick Staggs <nick.c.staggs@gmail.com>
2017-07-24 06:31:33 -0400

    added dependency for google analytics in appRun

--- public/js/appRun.js
+++ public/js/appRun.js
@@ -1,8 +1,8 @@
-angular.module('appRun', []).run(['$rootScope','$location','$anchorScroll','$routeParams',
-                          function($rootScope, $location, $anchorScroll, $routeParams) {
+angular.module('appRun', []).run(['$rootScope','$location','$anchorScroll','$routeParams', '$window',
+                          function($rootScope, $location, $anchorScroll, $routeParams, $window) {
 
 
-  ga('create', 'UA-102927018-1', 'auto');
+  $window.ga('create', 'UA-102927018-1', 'auto');
 
   $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
     $location.hash($routeParams.scrollTo);
