b27ae5e678cf7596fe2784f15a40b1232970fab8
Nick Staggs <nick.c.staggs@gmail.com>
2017-07-24 06:25:15 -0400

    Added google analytics

--- public/js/appRun.js
+++ public/js/appRun.js
@@ -1,8 +1,13 @@
 angular.module('appRun', []).run(['$rootScope','$location','$anchorScroll','$routeParams',
                           function($rootScope, $location, $anchorScroll, $routeParams) {
 
+
+  ga('create', 'UA-102927018-1', 'auto');
+
   $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
     $location.hash($routeParams.scrollTo);
     $anchorScroll();
+
+    $window.ga('send', 'pageview', $location.path());
   });
 }]);
