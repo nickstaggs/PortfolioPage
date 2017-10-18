72d8f1f7fc3c3518111af653b4f8183344538bd2
Nick Staggs <nick.c.staggs@gmail.com>
2017-10-18 17:01:04 -0400

    finished blogservice spec

--- client/app/blog/blog.service.spec.js
+++ client/app/blog/blog.service.spec.js
@@ -1,12 +1,13 @@
 'use strict';
 
 describe('BlogService', function() {
-  var BlogService, scope;
+  var BlogService, scope, httpBackend;
 
   beforeEach(angular.mock.module('app.blog'));
 
-  beforeEach(inject(function ($rootScope, $httpBackend, $q) {
+  beforeEach(inject(function ($rootScope, _$httpBackend_, $q) {
     scope = $rootScope.$new();
+    httpBackend = _$httpBackend_;
 
     inject(function($injector) {
       BlogService = $injector.get('BlogService');
@@ -15,5 +16,22 @@ describe('BlogService', function() {
 
   it('should exist', function() {
     expect(BlogService).toBeDefined();
+  });
+
+  it('should get blog posts', function() {
+    let returnData = "data";
+
+    httpBackend.expectGET("/api/blogPosts").respond(returnData);
+
+    let returnedPromise = BlogService.getBlogPosts();
+
+    let result;
+    returnedPromise.then(function(response) {
+      result = response;
+    });
+
+    httpBackend.flush();
+
+    expect(result).toEqual(returnData);
   });
 });
