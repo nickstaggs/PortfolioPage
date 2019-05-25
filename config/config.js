065060866f8ead4cab26cae5dd0f56b7fe0ab1f4
Nick Staggs <nick.c.staggs@gmail.com>
2019-05-25 15:45:49 -0400

    Removed connections to local mongo instance, created new cosmos connections

--- config/config.js
+++ config/config.js
@@ -5,23 +5,9 @@ var config = {};
 config.dbOptions = {};
 config.connectionOptions = {};
 
-
-config.dbOptions.readUser = process.env.readUser || 'read';
-config.dbOptions.readUserPass = process.env.readUserPass || 'password';
-config.dbOptions.readWriteUser = process.env.readWriteUser || 'readWrite';
-config.dbOptions.readWriteUserPass = process.env.readWriteUserPass || 'password';
-config.dbOptions.blogger = process.env.blogger || 'blogger';
-config.dbOptions.bloggerUserPass = process.env.bloggerUserPass || 'password';
-
-
-config.dbOptions.readUrl = 'mongodb://' + config.dbOptions.readUser + ":"
-                                        + config.dbOptions.readUserPass +'@localhost/portfolioDB';
-
-config.dbOptions.readWriteUrl = 'mongodb://' + config.dbOptions.readWriteUser + ":"
-                                             + config.dbOptions.readWriteUserPass +'@localhost/portfolioDB'
-
-config.dbOptions.bloggerUrl = 'mongodb://' + config.dbOptions.blogger + ":"
-                                           + config.dbOptions.bloggerUserPass +'@localhost/portfolioDB';
+config.dbOptions.readWriteUrl = process.env.mongoHost
+config.dbOptions.username = process.env.mongoUsername
+config.dbOptions.password = process.env.mongoPassword
 
 config.connectionOptions.httpPort = process.env.httpPort || '8000';
 config.connectionOptions.httpsPort = process.env.httpsPort || '8443';
